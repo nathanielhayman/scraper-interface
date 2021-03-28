8681dfe0b50611a32f4656efc338104e143c0d7e
Nathaniel Hayman <nathanielhayman@gmail.com>
2021-03-28 10:56:24 -0600

    Tweak starring functionality and fix #1

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -1,14 +1,39 @@
 window.onload=function(){
-    var fStar = document.querySelector('#sd-i-starred')
-    var eStar = document.querySelector('#sd-i-empty')
-    eStar.addEventListener('click', (e) => {
-        e.preventDefault();
-        fStar.style.display = "block";
-        eStar.style.display = "none";
+
+    var fStars = document.getElementsByClassName('sd-i-starred');
+    var eStars = document.getElementsByClassName('sd-i-empty');
+    Array.from(fStars).forEach(star => {
+        star.addEventListener('click', () => {
+            star.style.display = "none"
+            document.getElementById(`${star.id.substring(0, star.id.length - 12)}-empty-star`)
+            .style.display = "block";
+        });
     });
-    fStar.addEventListener('click', (e) => {
-        e.preventDefault();
-        eStar.style.display = "block";
-        fStar.style.display = "none";
+    Array.from(eStars).forEach(star => {
+        star.addEventListener('click', () => {
+            console.log(star.id);
+            star.style.display = "none"
+            document.getElementById(`${star.id.substring(0, star.id.length - 11)}-filled-star`)
+            .style.display = "block";
+        });
     });
 }
+
+// fix turbolinks error
+var fStars = document.getElementsByClassName('sd-i-starred');
+    var eStars = document.getElementsByClassName('sd-i-empty');
+    Array.from(fStars).forEach(star => {
+        star.addEventListener('click', () => {
+            star.style.display = "none"
+            document.getElementById(`${star.id.substring(0, star.id.length - 12)}-empty-star`)
+            .style.display = "block";
+        });
+    });
+    Array.from(eStars).forEach(star => {
+        star.addEventListener('click', () => {
+            console.log(star.id);
+            star.style.display = "none"
+            document.getElementById(`${star.id.substring(0, star.id.length - 11)}-filled-star`)
+            .style.display = "block";
+        });
+    });
