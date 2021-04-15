3690e213eddc7af5c81569c05c9c42f8f73b2daf
Nathaniel Hayman <nathanielhayman@gmail.com>
2021-04-15 11:11:43 -0600

    Javascript components for countdown

--- app/javascript/packs/tasks.js
+++ app/javascript/packs/tasks.js
@@ -1,33 +1,33 @@
 function initializeComponents() {
-    var fStars = document.getElementsByClassName('sd-i-starred');
-    var eStars = document.getElementsByClassName('sd-i-empty');
-    Array.from(fStars).forEach(star => {
-        star.addEventListener('click', () => {
-            star.style.display = "none"
-            document.getElementById(`${star.id.substring(0, star.id.length - 12)}-empty-star`)
-            .style.display = "block";
-        });
-    });
-    Array.from(eStars).forEach(star => {
-        star.addEventListener('click', () => {
-            star.style.display = "none"
-            document.getElementById(`${star.id.substring(0, star.id.length - 11)}-filled-star`)
-            .style.display = "block";
-        });
-    });
-    var clocks = document.getElementsByClassName('task-clock');
+    var toggles = document.getElementsByClassName('toggle');
+    console.log(toggles);
+    console.log("a");
+    Array.from(toggles).forEach(toggle => {
+        toggle.addEventListener('click', () => {
+            if (toggle.id.includes("toggled")) {
+                toggle.childNodes[1].style.right = "";
+                toggle.id = `${toggle.id.replace("-toggled", "")}`;
+            } else {
+                toggle.childNodes[1].style.right = 0;
+                toggle.id = `${toggle.id}-toggled`;
+            }
+        })
+    })
+    var clocks = document.getElementsByClassName('t-clock');
     Array.from(clocks).forEach(clock => {
         var txt = clock.getElementsByTagName('b')[0];
         var input_str = clock.innerText.slice(clock.innerText.indexOf("—")+1, -1);
         var mod = input_str.slice(0, input_str.lastIndexOf(":")+3);
         var hms = mod.split(":").map(x => +x);
         var p = input_str.slice(input_str.lastIndexOf(":")+4, input_str.indexOf("M")+1);
+        console.log(p, hms);
         if (p == "PM") hms[0] += 12;
         var today = new Date();
         var nextExec = new Date(today.getFullYear(), today.getMonth(), today.getDate(), hms[0], hms[1], hms[2]);
         setInterval(() => { 
             var now = new Date();
             var diff = nextExec - now;
+            //console.log(`${nextExec} - ${now} = ${diff}`);
             var seconds = diff / 1000;
             var hours = parseInt( seconds / 3600 );
             var minutes = parseInt( seconds / 60 );
