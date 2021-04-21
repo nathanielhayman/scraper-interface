dea4f910d4f6d8e3fb485fe8c0ccfc85585ca520
Nathaniel Hayman <nathanielhayman@gmail.com>
2021-04-21 17:49:21 -0600

    Add AJAX support to dashboard starring system and clocks.

--- app/javascript/packs/dashboard.js
+++ app/javascript/packs/dashboard.js
@@ -2,43 +2,85 @@ function initializeComponents() {
     var fStars = document.getElementsByClassName('sd-i-starred');
     var eStars = document.getElementsByClassName('sd-i-empty');
     Array.from(fStars).forEach(star => {
+        var short = star.id.substring(0, star.id.length - 12);
         star.addEventListener('click', () => {
             star.style.display = "none"
-            document.getElementById(`${star.id.substring(0, star.id.length - 12)}-empty-star`)
+            document.getElementById(`${short}-empty-star`)
             .style.display = "block";
+            $.ajax({
+                url: `/tasks/star/${short}`,
+                type: 'post',
+                data: {starred: false},
+                success: () => {
+                    
+                },
+                error: () => {
+                    throw("Unable to contact server!");
+                }
+            });
         });
     });
     Array.from(eStars).forEach(star => {
+        var short = star.id.substring(0, star.id.length - 11);
         star.addEventListener('click', () => {
             star.style.display = "none"
-            document.getElementById(`${star.id.substring(0, star.id.length - 11)}-filled-star`)
+            document.getElementById(`${short}-filled-star`)
             .style.display = "block";
+            $.ajax({
+                url: `/tasks/star/${short}`,
+                type: 'post',
+                data: {starred: true},
+                success: () => {
+                    
+                },
+                error: () => {
+                    throw("Unable to contact server!");
+                }
+            });
         });
     });
-    var clocks = document.getElementsByClassName('task-clock');
-    Array.from(clocks).forEach(clock => {
-        var txt = clock.getElementsByTagName('b')[0];
-        var input_str = clock.innerText.slice(clock.innerText.indexOf("—")+1, -1);
-        var mod = input_str.slice(0, input_str.lastIndexOf(":")+3);
-        var hms = mod.split(":").map(x => +x);
-        var p = input_str.slice(input_str.lastIndexOf(":")+4, input_str.indexOf("M")+1);
-        if (p == "PM") hms[0] += 12;
-        var today = new Date();
-        var nextExec = new Date(today.getFullYear(), today.getMonth(), today.getDate(), hms[0], hms[1], hms[2]);
-        setInterval(() => { 
-            var now = new Date();
-            var diff = nextExec - now;
-            var seconds = diff / 1000;
-            var hours = parseInt( seconds / 3600 );
-            var minutes = parseInt( seconds / 60 );
-            minutes = parseInt(minutes % 60);
-            seconds = parseInt(seconds % 60);
-            txt.innerHTML = `${hours ? hours + ":" : ""}${minutes < 10 ? "0" + minutes + ":" : minutes + ":"}${seconds < 10 ? "0" + seconds : seconds}`; 
-        }, 1000);
+    var containers = document.getElementsByClassName("sd-card-container");
+    Array.from(containers).forEach(container => {
+        var clock = container.getElementsByClassName("task-clock")[0];
+        var short = container.getElementsByClassName("task-short-report")[0].id;
+        $.ajax({
+            url: `/tasks/api/${short}`,
+            type: 'get',
+            success: (tsk) => {
+                updateClock(tsk, clock);
+            },
+            error: () => {
+                throw("Unable to contact server!");
+            }
+        });
     });
 }
 
-window.onload=function(){
+function updateClock(tsk, clock) {
+    var txt = clock.getElementsByTagName('b')[0];
+    var nextExec = new Date(tsk.time);
+    setInterval(() => { 
+        var now = new Date();
+        if (now.getHours() > nextExec.getHours())  {
+            nextExec.setDate(now.getDate()+1);
+        }
+        if(now.getDate() > nextExec.getDate()) {
+            nextExec.setDate(now.getDate());
+        }
+        var diff = nextExec - now;
+        var seconds = diff / 1000;
+        var hours = parseInt( seconds / 3600 );
+        var minutes = parseInt( seconds / 60 );
+        minutes = parseInt(minutes % 60);
+        seconds = parseInt(seconds % 60);
+        txt.innerHTML = `${hours ? hours + ":" : ""}${minutes < 10 ? "0" + minutes + ":" : minutes + ":"}${seconds < 10 ? "0" + seconds : seconds}`; 
+    }, 1000);
+    //setInterval(() => {
+    //    
+    //});
+}
+
+window.onload=function() {
     initializeComponents();
 }
 
