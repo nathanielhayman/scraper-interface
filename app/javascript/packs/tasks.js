6e577458be9f63a7a99a093ac847dbb46948c551
Nathaniel Hayman <nathanielhayman@gmail.com>
2021-04-21 19:04:45 -0600

    Add AJAX support to task status and add a rudimentary console

--- app/javascript/packs/tasks.js
+++ app/javascript/packs/tasks.js
@@ -1,63 +1,81 @@
 function cascadeChangeMethods(on) {
-    var methods = document.getElementsByClassName('t-activity-bar');
+    var methods;
+    if (on) { 
+        var methods = document.getElementsByClassName('t-activity-bar-s');
+    } else { 
+        var methods = document.getElementsByClassName('t-activity-bar-r');
+    }
+    console.log(methods);
     Array.from(methods).forEach(method => {
         setTimeout(() => {
             if (on) {
-                method.style.backgroundColor = "rgb(85, 228, 104)";
+                method.classList.add("t-activity-bar-r");
+                method.classList.remove("t-activity-bar-s");
             } else {
-                method.style.backgroundColor = "";
+                method.classList.add("t-activity-bar-s");
+                method.classList.remove("t-activity-bar-r");
             }
         }, 100);
     });
 }
 
 function initializeComponents() {
+    var element = document.getElementById("console-output");
+    var short = $('.task-short-report').attr('id');
+    element.scrollTop = element.scrollHeight ;
     var toggles = document.getElementsByClassName('toggle');
     var running = document.getElementsByClassName('running-confirm')[0];
     var stopped = document.getElementsByClassName('stopped-confirm')[0];
-    console.log(toggles);
-    console.log("a");
     Array.from(toggles).forEach(toggle => {
         toggle.addEventListener('click', () => {
             if (toggle.id.includes("toggled")) {
-                toggle.childNodes[1].style.left = "";
-                toggle.style.backgroundColor = "";
                 running.style.display = "none";
                 stopped.style.display = "block";
                 toggle.id = `${toggle.id.replace("-toggled", "")}`;
                 cascadeChangeMethods(false);
+                $.ajax({
+                    url: `/tasks/toggle/${short}`,
+                    type: 'post',
+                    data: {short: short, status: "Stopped"},
+                    success: () => {
+                        $('input').val("");
+                    },
+                    error: () => {
+                        throw("Unable to contact server!");
+                    }
+                });
             } else {
-                toggle.childNodes[1].style.left = "53%";
-                toggle.style.backgroundColor = "rgb(85, 228, 104)";
                 running.style.display = "block";
                 stopped.style.display = "none";
                 toggle.id = `${toggle.id}-toggled`;
                 cascadeChangeMethods(true);
+                $.ajax({
+                    url: `/tasks/toggle/${short}`,
+                    type: 'post',
+                    data: {short: short, status: "Running"},
+                    success: () => {
+                        $('input').val("");
+                    },
+                    error: () => {
+                        throw("Unable to contact server!");
+                    }
+                });
             }
-        })
-    })
-    var clocks = document.getElementsByClassName('t-clock');
-    Array.from(clocks).forEach(clock => {
-        var txt = clock.getElementsByTagName('b')[0];
-        var input_str = clock.innerText.slice(clock.innerText.indexOf("—")+1, -1);
-        var mod = input_str.slice(0, input_str.lastIndexOf(":")+3);
-        var hms = mod.split(":").map(x => +x);
-        var p = input_str.slice(input_str.lastIndexOf(":")+4, input_str.indexOf("M")+1);
-        console.log(p, hms);
-        if (p == "PM") hms[0] += 12;
-        var today = new Date();
-        var nextExec = new Date(today.getFullYear(), today.getMonth(), today.getDate(), hms[0], hms[1], hms[2]);
-        setInterval(() => { 
-            var now = new Date();
-            var diff = nextExec - now;
-            //console.log(`${nextExec} - ${now} = ${diff}`);
-            var seconds = diff / 1000;
-            var hours = parseInt( seconds / 3600 );
-            var minutes = parseInt( seconds / 60 );
-            minutes = parseInt(minutes % 60);
-            seconds = parseInt(seconds % 60);
-            txt.innerHTML = `${hours ? hours + ":" : ""}${minutes < 10 ? "0" + minutes + ":" : minutes + ":"}${seconds < 10 ? "0" + seconds : seconds}`; 
-        }, 1000);
+        });
+    });
+    $('form').on("submit", () => {
+        $.ajax({
+            url: `/tasks/api/${short}`,
+            type: 'get',
+            success: (tsk) => {
+                $('input').val("");
+                location.reload();
+                console.log(tsk);
+            },
+            error: () => {
+                throw("Unable to contact server!");
+            }
+        });
     });
 }
 
